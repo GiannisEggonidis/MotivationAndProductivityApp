0b54b9f823e8685ea73ce7c3044d59ddbf3b1b88
GiannisEggonidis <gianniseggonidis@gmail.com>
2022-05-19 12:54:44 +0300

    Notification title and text now change with notificationName changes

--- app/src/main/java/com/ioannis_engonidis_thesis/motivationandproductivityapp/NotificationPanelReceiver.java
+++ app/src/main/java/com/ioannis_engonidis_thesis/motivationandproductivityapp/NotificationPanelReceiver.java
@@ -1,26 +1,33 @@
 package com.ioannis_engonidis_thesis.motivationandproductivityapp;
 
 import android.app.Notification;
+import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 
+import androidx.annotation.RequiresApi;
 import androidx.core.app.NotificationCompat;
 
 public class NotificationPanelReceiver extends BroadcastReceiver {
-    private String title = "test", channelID = "1";
+    private String  channelID = "1";
     private int notificationID = 1;
 
+    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     public void onReceive(Context context, Intent intent) {
         NotificationManager manager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
-
         channelID = String.valueOf(intent.getIntExtra("notificationID",1));
+        NotificationChannel channel =manager.getNotificationChannel(channelID);
+        String channelName = (String) channel.getName();
+
+
         Notification notification = new NotificationCompat.Builder(context,channelID )
                 .setSmallIcon(R.drawable.ic_repeating_reminder_notification_icon)
-                .setContentTitle(intent.getStringExtra("title"))
-                .setContentText(intent.getStringExtra("title"))
+                .setContentTitle(channelName)
+                .setContentText(channelName)
                 .build();
 
         manager.notify(intent.getIntExtra("notificationID", notificationID), notification);
